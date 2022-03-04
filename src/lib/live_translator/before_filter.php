d84416ce2cb34d0cf83aac6041bcbaff8b61f772
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-03-04 10:59:37 +0100

    Comment added

--- src/lib/live_translator/before_filter.php
+++ src/lib/live_translator/before_filter.php
@@ -9,12 +9,15 @@ class BeforeFilter {
 		];
 
 		$back_replaces = [];
-
 		$uniqid = $options["uniqid"];
-		$counter = 1;
 
+		// The following replace markdown shortcuts [row][/row], [col][/col] and [div][/div]
+		// to HTML tags <div class="...."></div>
+		// While Deepl.com leaves HTML tags intact, Google Translator changes them.
+		// So it's not suitable for Google Translator!
 		$well_known_markdown_shortcodes = ["div","row","col"];
 		$well_known_markdown_shortcodes = "(".join("|",$well_known_markdown_shortcodes).")";
+		$counter = 1;
 		$source_text = preg_replace_callback(
 			'/(\['.$well_known_markdown_shortcodes.'\b[^\]]*\]|\[\/'.$well_known_markdown_shortcodes.'\])/',
 			function($matches) use($uniqid,&$counter,&$back_replaces){
